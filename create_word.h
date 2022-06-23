cca72e9e7a2bfc64a6a7c501fa00887b6495fe2f
lauj32 <105667937+lauj32@users.noreply.github.com>
2022-06-23 00:11:56 -0300

    Create create_word.h

---
+++ create_word.h
@@ -0,0 +1,18 @@
+#ifndef _CREATE_WORD_H
+#define _CREATE_WORD_H
+
+
+short search_letter(const char * , char );
+
+short find_word(char * , char *);
+
+void add_letter(char ** , char);
+
+void add_letterV2(char ** , char);
+
+void delete_letter(char ** , char);
+
+void delete_word(char **);
+
+
+#endif
